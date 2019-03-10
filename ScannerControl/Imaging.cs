8564b8a96ff195e360b35aa03d396f250b37d34b
DLMousey <tom@deadlyllama.com>
2019-03-10 01:43:11 +0000

    Retrieve first available scanner via WIA

---
+++ ScannerControl/Imaging.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WIA;
+
+namespace ScannerControl
+{
+    public class Imaging
+    {
+        public ImagingDevice ActiveDevice;
+
+        public Imaging()
+        {
+            DeviceManager DeviceManager = new DeviceManager();
+            for (int i = 1; i <= DeviceManager.DeviceInfos.Count; i++)
+            {
+                if (DeviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                    continue;
+
+                DeviceInfo CurrentDevice = DeviceManager.DeviceInfos[i];
+                this.ActiveDevice = new ImagingDevice
+                {
+                    Name = CurrentDevice.Properties["Name"].get_Value().ToString(),
+                    Description = CurrentDevice.Properties["Description"].get_Value().ToString(),
+                    Port = CurrentDevice.Properties["Port"].get_Value().ToString()
+                };
+
+                Console.WriteLine(this.ActiveDevice.ToString());
+            }
+        }
+    }
+}
---
+++ ScannerControl/ImagingDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerControl
+{
+    public class ImagingDevice
+    {
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+        public String Port { get; set; }
+
+        public override string ToString()
+        {
+            return "Device Name : " + this.Name + " | Description : " + this.Description + " | Port : " + this.Port;
+        }
+    }
+}
